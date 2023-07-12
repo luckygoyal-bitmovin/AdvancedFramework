2d99ec0c0c1157401934b5158e8ac788beb46748
Lucky Goyal <lucky.goyal@bitmovin.com>
2023-07-12 15:25:01 +0200

    Sample Library for SPM test

---
+++ AdvancedFramework/PlayerWrapper.swift
@@ -0,0 +1,55 @@
+//
+// Bitmovin Player iOS SDK
+// Copyright (C) 2023, Bitmovin GmbH, All Rights Reserved
+//
+// This source code and its use and distribution, is subject to the terms
+// and conditions of the applicable license agreement.
+//
+
+import BitmovinPlayer
+import BitmovinCollector
+import CoreCollector
+import GoogleCast
+
+// This sample showcases how the BitmovinPlayer could be used from within another Framework which wraps the Player.
+public class PlayerWrapper {
+    private let player: Player
+    private let collector: BitmovinPlayerCollector
+    
+    private var bitmovinCastManager: BitmovinCastManager
+    // Bitmovin cast manger options
+    private let bitmovinCastManagerOptions = BitmovinCastManagerOptions()
+    // The namesapce for the message to send/receive. namespace must begin with "urn:x-cast:"
+    private static var namespace = "urn:x-cast:com.mediakind.cast.media"
+    // ID to communicate with receiver
+    private static var applicationId = "5A468DEE"
+    
+
+    public init() {
+        let playerConfig = PlayerConfig()
+        playerConfig.key = "your license key"
+
+        let analyticsConfig = BitmovinAnalyticsConfig(key: "your analytics key")
+
+        self.player = PlayerFactory.create(playerConfig: playerConfig)
+        self.collector = BitmovinPlayerCollector(config: analyticsConfig)
+        
+        bitmovinCastManagerOptions.deviceDiscoveryMode = .castButtonInteraction
+        bitmovinCastManagerOptions.applicationId = "5A468DEE"
+        bitmovinCastManagerOptions.messageNamespace = PlayerWrapper.namespace
+        bitmovinCastManagerOptions.enableBackgroundSessions = false
+        BitmovinCastManager.initializeCasting(options: bitmovinCastManagerOptions)
+        bitmovinCastManager = BitmovinCastManager.sharedInstance()
+        let currentMediaStatus = bitmovinCastManager.currentMediaStatus
+    }
+
+    func setup() {
+        collector.attachPlayer(player: player)
+    }
+
+    func play() {
+        player.play()
+    }
+    
+    
+}
